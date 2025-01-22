70231d5a1bc936d3a1321076cdb7c20aff0256bc
manou422 <162481118+manou422@users.noreply.github.com>
2025-01-22 15:36:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,8 +76,6 @@ def enregistrer_client():
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
-
-@app.route('/fiche_nom/')
-                                                                                                                                       
+                                                                                                           
 if __name__ == "__main__":
   app.run(debug=True)
