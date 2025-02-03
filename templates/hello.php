1708e67084dcc782807e299f069a7f78d83b9405
manou422 <162481118+manou422@users.noreply.github.com>
2025-02-03 17:38:49 +0100

    Update hello.php

--- templates/hello.php
+++ templates/hello.php
@@ -1,12 +1,7 @@
 <!doctype html>
 <title>Flask Server</title>
-<h1> Bienvenue sur la bibliothèque </h1>
-<button onclick="afficherMessage()">Cliquez-moi</button>
-<p id="message"></p>
 
-<script>
-function afficherMessage() {
-    document.getElementById("message").innerText = "Bouton cliqué !";
-}
-</script>
+<a href="utilisateur.html">
+    <button>Aller à la page 2</button>
+</a>
 
